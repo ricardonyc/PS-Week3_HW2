03a9e5698e26147ad4aabaf5adde3a90248b269a
Ricardo Vargas <rvardo98@gmail.com>
2021-12-12 12:30:24 -0500

    finished first 3 exercises

--- fToC.js
+++ fToC.js
@@ -1,13 +1,14 @@
 // Fahrenheit to Celsius
 
-const output2 = document.getElementById("c-output");
+const celsiusOutput = document.getElementById("c-output");
 
 const fahrenheitToCelsius = () => {
   const fValue = document.getElementById("f").value;
   if (!fValue) {
-    output2.innerHTML = "Please enter a number";
+    celsiusOutput.innerHTML = "Please enter a number";
   } else if (fValue) {
-    let c = Math.floor(((fValue - 32) * 5) / 9);
-    output2.innerHTML = `${fValue} degrees Fahrenheit is ${c} degrees Celsius`;
+    const c = Math.floor(((fValue - 32) * 5) / 9);
+    celsiusOutput.innerHTML = `${fValue} degrees Fahrenheit is ${c} degrees Celsius`;
   }
 };
+ 
--- largestNum.js
+++ largestNum.js
@@ -1,8 +1,35 @@
 // 1. Define a function maxOfThree that takes three numbers as arguments and returns the largest of them.
+const largestNumOutput = document.getElementById("num-output");
 
-const maxOfThree = (num1, num2, num3) => {
-    let nums = [num1, num2, num3]
-    let largestNum = nums.reduce((a, b) => a > b ? a : b)
-    return largestNum
+function largestNum() {
+  const numInput = document.getElementById("num-input").value;
+  let newer = numInput.split(",").map((x) => Number(x));
+  let newest = newer.reduce((a, b) => (a > b ? a : b));
+  if (!numInput) {
+    largestNumOutput.innerHTML = `Please enter a number :(`;
+  } else {
+    largestNumOutput.innerHTML = `The largest number is: ${newest}`;
+  }
 }
 
+
+
+
+
+
+// const wishlist = [
+//     { title: "Tesla Model S", price: 90000 },
+//     { title: "4 carat diamond ring", price: 45000 },
+//     { title: "Fancy hacky Sack", price: 5 },
+//     { title: "Gold fidgit spinner", price: 2000 },
+//     { title: "A second Tesla Model S", price: 90000 }
+//   ];
+
+//   const shoppingSpree = (inWishlist) => {
+//     const totalCost = inWishlist.reduce((a, b) => {
+//       return a + b.price
+//     }, 0);
+//     return totalCost;
+//   }
+
+//   console.log(shoppingSpree(wishlist))
